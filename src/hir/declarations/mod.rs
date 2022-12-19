c04b1f58ba66576fdb3cd1d5062b2532ea2f9a32
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2022-12-19 17:06:00 +0800

    Impl Named for Declaration

--- src/hir/declarations/mod.rs
+++ src/hir/declarations/mod.rs
@@ -11,10 +11,22 @@ use derive_more::{From, TryInto};
 
 use std::sync::Arc;
 
+use crate::named::Named;
+
 /// Any PPL declaration
 #[derive(Debug, PartialEq, Eq, Clone, From, TryInto)]
 pub enum Declaration {
 	Variable(Arc<VariableDeclaration>),
 	Type(Arc<TypeDeclaration>),
 	Function(Arc<FunctionDeclaration>),
+}
+
+impl Named for Declaration {
+	fn name(&self) -> &str {
+		match self {
+			Declaration::Variable(decl) => decl.name(),
+			Declaration::Type(decl) => decl.name(),
+			Declaration::Function(decl) => decl.name(),
+		}
+	}
 }
