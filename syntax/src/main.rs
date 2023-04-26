4e164d88629300c91a27045ec40180057b3d9a58
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-04-26 08:27:20 +0800

    Use nom finish

--- syntax/src/main.rs
+++ syntax/src/main.rs
@@ -1,16 +1,12 @@
+use nom::Finish;
 use syntax::parsers::rule;
 
 fn main() {
     loop {
         let mut line = String::new();
         std::io::stdin().read_line(&mut line).unwrap();
-        let res = rule(&line);
+        let res = rule(&line).finish();
         if let Err(err) = res {
-            let err = match err {
-                nom::Err::Error(e) => e,
-                nom::Err::Failure(e) => e,
-                nom::Err::Incomplete(_) => unreachable!(),
-            };
             println!("{:?}", err);
             continue;
         }
