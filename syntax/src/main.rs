7f98d8fda6e8a9d8eef4741148f55435ee6a44c6
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-04-26 08:24:05 +0800

    Update printing

--- syntax/src/main.rs
+++ syntax/src/main.rs
@@ -4,6 +4,18 @@ fn main() {
     loop {
         let mut line = String::new();
         std::io::stdin().read_line(&mut line).unwrap();
-        println!("{:?}", rule(&line))
+        let res = rule(&line);
+        if let Err(err) = res {
+            let err = match err {
+                nom::Err::Error(e) => e,
+                nom::Err::Failure(e) => e,
+                nom::Err::Incomplete(_) => unreachable!(),
+            };
+            println!("{:?}", err);
+            continue;
+        }
+
+        let (_rest, (_tree, ast)) = res.unwrap();
+        println!("{:?}", ast)
     }
 }
