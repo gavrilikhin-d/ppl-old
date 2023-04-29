fbe0bf3822ef155cb904c3483981305691104c62
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-04-29 10:13:23 +0800

    Add new functions to context

--- syntax/src/context.rs
+++ syntax/src/context.rs
@@ -1,4 +1,4 @@
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 
 use crate::Rule;
 
@@ -9,7 +9,7 @@ static CONTEXT: Mutex<Context> = Mutex::new(Context { rules: vec![] });
 #[derive(Default)]
 pub struct Context {
     /// Parsing rules
-    pub rules: Vec<Rule>,
+    pub rules: Vec<Arc<Rule>>,
 }
 
 /// Get the current parsing context
@@ -20,5 +20,10 @@ pub fn with_context<T>(f: impl FnOnce(&mut Context) -> T) -> T {
 
 /// Add a rule to the current parsing context
 pub fn add_rule(rule: Rule) {
-    with_context(|c| c.rules.push(rule))
+    with_context(|c| c.rules.push(Arc::new(rule)))
+}
+
+/// Find rule by name in the current parsing context
+pub fn find_rule(name: &str) -> Option<Arc<Rule>> {
+    with_context(|c| c.rules.iter().find(|r| r.name == name).map(|r| r.clone()))
 }
