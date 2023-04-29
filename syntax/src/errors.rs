42fc56463be19e81ef5addcd0c4d0f566c04f5ed
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-04-29 10:13:29 +0800

    Add new errors

--- syntax/src/errors.rs
+++ syntax/src/errors.rs
@@ -1,15 +1,78 @@
+use derive_more::From;
+use thiserror::Error;
+
 /// Creates recoverable error for nom
 #[macro_export]
 macro_rules! err {
+    ($error: expr) => {
+        Err(nom::Err::Error($error))
+    };
+}
+
+/// Creates boxed recoverable error for nom
+#[macro_export]
+macro_rules! err_boxed {
     ($error: expr) => {
         Err(nom::Err::Error(Box::new($error)))
     };
 }
 
-#[derive(Debug, thiserror::Error)]
+/// Creates boilerplate for error enum
+macro_rules! error_enum {
+	($name:ident, $($variant:ident)|*) => {
+		#[derive(Debug, Error, From, PartialEq, Eq)]
+		pub enum $name<'i> {
+			$(
+				#[error(transparent)]
+				$variant($variant<'i>),
+			)*
+		}
+	};
+}
+
+#[derive(Debug, Error, PartialEq, Eq)]
 #[error("Regex didn't match")]
 pub struct RegexMismatch {}
 
-#[derive(Debug, thiserror::Error)]
-#[error("Unknown rule reference")]
-pub struct UnknownRuleReference {}
+#[derive(Debug, Error, PartialEq, Eq)]
+#[error("expected rule name")]
+pub struct ExpectedRuleName<'i> {
+    /// The input that caused the error
+    pub at: &'i str,
+}
+
+#[derive(Debug, Error, PartialEq, Eq)]
+#[error("lowercase rule name")]
+pub struct LowercaseRuleName<'i> {
+    /// The rule name that caused the error
+    pub name: &'i str,
+}
+
+error_enum!(RuleNameError, ExpectedRuleName | LowercaseRuleName);
+
+#[derive(Debug, Error, PartialEq, Eq)]
+#[error("unknown rule reference")]
+pub struct UnknownRuleReference<'i> {
+    /// The rule name that caused the error
+    pub name: &'i str,
+}
+
+error_enum!(RuleReferenceError, RuleNameError | UnknownRuleReference);
+
+#[derive(Debug, Error, PartialEq, Eq)]
+#[error("expected regex")]
+pub struct ExpectedRegex<'i> {
+    /// The input that caused the error
+    pub at: &'i str,
+}
+
+#[derive(Debug, Error, PartialEq, Eq)]
+#[error("invalid regex: ${reason}")]
+pub struct InvalidRegex<'i> {
+    /// Location of the invalid regex
+    pub re: &'i str,
+    /// The reason why the regex is invalid
+    pub reason: String,
+}
+
+error_enum!(RegexError, ExpectedRegex | InvalidRegex);
