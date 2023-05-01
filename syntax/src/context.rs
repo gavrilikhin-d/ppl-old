cf6767e016da74b8b6b151b89b965e805fcf0504
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-05-01 11:39:38 +0800

    Add Typename rule

--- syntax/src/context.rs
+++ syntax/src/context.rs
@@ -25,10 +25,16 @@ pub struct Context {
 impl Default for Context {
     fn default() -> Self {
         Context {
-            rules: vec![Arc::new(Rule {
-                name: "Regex".to_string(),
-                patterns: vec![r"[^\s]+".into()],
-            })],
+            rules: vec![
+                Arc::new(Rule {
+                    name: "Regex".to_string(),
+                    patterns: vec![r"[^\s]+".into()],
+                }),
+                Arc::new(Rule {
+                    name: "Typename".to_string(),
+                    patterns: vec![r"[A-Z][a-zA-Z0-9_]*".into()],
+                }),
+            ],
             on_parse: HashMap::new(),
         }
     }
@@ -55,4 +61,37 @@ pub fn on_parse(name: &str, action: impl OnParseAction + 'static) {
     with_context(|c| {
         c.on_parse.insert(name.to_string(), Box::new(action));
     })
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{
+        context,
+        errors::Expected,
+        parsers::{ParseResult, Parser},
+        ParseTree,
+    };
+
+    #[test]
+    fn default_rules() {
+        let typename = context::find_rule("Typename").unwrap();
+        assert_eq!(typename.name, "Typename");
+        assert_eq!(
+            typename.parse("Foo"),
+            ParseResult {
+                delta: 3,
+                tree: ParseTree::named("Typename").with("Foo"),
+            }
+        );
+        assert_eq!(
+            typename.parse("foo"),
+            ParseResult {
+                delta: 0,
+                tree: ParseTree::named("Typename").with(Expected {
+                    expected: "[A-Z][a-zA-Z0-9_]*".to_string(),
+                    at: 0.into()
+                })
+            }
+        );
+    }
 }
