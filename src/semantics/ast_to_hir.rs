e95357325ebe46951cc988b8a05705068fb0d269
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-10-07 12:57:04 +0800

    Add test for generics (currently failing)

--- src/semantics/ast_to_hir.rs
+++ src/semantics/ast_to_hir.rs
@@ -920,6 +920,7 @@ mod tests {
     use crate::test_compiler_error;
 
     test_compiler_error!(candidate_not_viable);
+    test_compiler_error!(generics);
     test_compiler_error!(missing_fields);
     test_compiler_error!(multiple_initialization);
     test_compiler_error!(wrong_initializer_type);
