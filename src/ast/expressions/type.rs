b2a4e2e1db7f51197127b2af7c80811ee806ee32
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-10-14 21:30:11 +0800

    TypeReference should start with capital letter

--- src/ast/expressions/type.rs
+++ src/ast/expressions/type.rs
@@ -17,7 +17,10 @@ pub struct TypeReference {
 impl StartsHere for TypeReference {
     /// Check that type reference may start at current lexer position
     fn starts_here(context: &mut Context<impl Lexer>) -> bool {
-        context.lexer.try_match(Token::Id).is_ok()
+        context
+            .lexer
+            .try_match(Token::Id)
+            .is_ok_and(|t| t.as_str().chars().nth(0).is_some_and(|c| c.is_uppercase()))
     }
 }
 
