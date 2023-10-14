7edaa21750042f3f89e3e0e4b38bcfd8200c7c07
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-10-14 20:59:39 +0800

    Store `Arc<NamedSource>` inside `SourceFile`

--- src/source_file.rs
+++ src/source_file.rs
@@ -1,46 +1,60 @@
 use std::{
     fs, io,
     path::{Path, PathBuf},
+    sync::Arc,
 };
 
 use miette::{MietteError, NamedSource, SourceCode, SpanContents};
 
 /// Wrapper around [`PathBuf`] that implements [`SourceCode`]
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct SourceFile {
     /// Path to file
     path: PathBuf,
     /// File contents
-    source: NamedSource,
-}
-
-impl Clone for SourceFile {
-    fn clone(&self) -> Self {
-        Self::with_path(&self.path).unwrap()
-    }
+    source: Arc<NamedSource>,
 }
 
 impl PartialEq for SourceFile {
     fn eq(&self, other: &Self) -> bool {
-        self.path == other.path
+        self.path == other.path && self.source.name() == other.source.name()
     }
 }
 
+impl Eq for SourceFile {}
+
 impl SourceFile {
+    /// Get virtual source file
+    pub fn in_memory(source: NamedSource) -> Self {
+        Self {
+            path: PathBuf::new(),
+            source: Arc::new(source),
+        }
+    }
+
     /// Wrap path to source file
     pub fn with_path(path: impl Into<PathBuf>) -> io::Result<Self> {
         let path = path.into();
-        let name = path.file_name().unwrap().to_string_lossy().to_string();
+        let name = path
+            .file_name()
+            .expect(format!("Can't get filename of `{}`", path.display()).as_str())
+            .to_string_lossy()
+            .to_string();
         let source = fs::read_to_string(&path)?;
         Ok(Self {
             path,
-            source: NamedSource::new(name, source),
+            source: Arc::new(NamedSource::new(name, source)),
         })
     }
 
     /// Get path to the source file
     pub fn path(&self) -> &Path {
         &self.path
+    }
+
+    /// Name of the source file
+    pub fn name(&self) -> &str {
+        self.source.name()
     }
 }
 
