d0574db61d8dda729059bfcd22a2106d78cf983a
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-10-28 20:55:39 +0800

    Remove unmaintained subprojects (#14)

--- driver/src/commands/execute.rs
+++
@@ -1,8 +0,0 @@
-/// Trait for executing commands
-pub trait Execute {
-    /// The return type of the command
-    type ReturnType = ();
-
-    /// Execute the command
-    fn execute(&self) -> Self::ReturnType;
-}
