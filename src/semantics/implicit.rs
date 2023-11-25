422846ca7125e8d88a596c4482239bb5d726e615
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-11-25 17:03:15 +0800

    Add helpers to reference/dereference

---
+++ src/semantics/implicit.rs
@@ -0,0 +1,48 @@
+use crate::hir::{Expression, ImplicitConversion, ImplicitConversionKind::*, Typed};
+
+use super::Context;
+
+/// Trait to wrap expression with implicit dereference/reference
+pub trait Implicit {
+    /// Implicitly dereference this expression
+    fn dereference(self) -> Self;
+
+    /// Implicitly reference this expression
+    fn reference(self, context: &impl Context) -> Self;
+
+    /// Implicitly reference this with mutable reference
+    fn reference_mut(self, context: &impl Context) -> Self;
+}
+
+impl Implicit for Expression {
+    fn dereference(self) -> Self {
+        if !self.is_reference() {
+            return self;
+        }
+
+        ImplicitConversion {
+            kind: Dereference,
+            ty: self.ty().without_ref(),
+            expression: Box::new(self),
+        }
+        .into()
+    }
+
+    fn reference(self, context: &impl Context) -> Self {
+        ImplicitConversion {
+            kind: Reference,
+            ty: context.builtin().types().reference_to(self.ty()),
+            expression: Box::new(self),
+        }
+        .into()
+    }
+
+    fn reference_mut(self, context: &impl Context) -> Self {
+        ImplicitConversion {
+            kind: Reference,
+            ty: context.builtin().types().reference_mut_to(self.ty()),
+            expression: Box::new(self),
+        }
+        .into()
+    }
+}
