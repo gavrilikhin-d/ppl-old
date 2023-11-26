086f7464af4334486d3c96448b8e7a28939aacc4
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-11-26 17:27:07 +0800

    Print ret with indent

--- src/hir/statements/ret.rs
+++ src/hir/statements/ret.rs
@@ -11,6 +11,9 @@ pub struct Return {
 
 impl Display for Return {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let indent = "\t".repeat(f.width().unwrap_or(0));
+        write!(f, "{indent}")?;
+
         if let Some(value) = &self.value {
             write!(f, "return {}", value)
         } else {
