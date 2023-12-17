f3614120323a8619c4ba18a39911ec5d33fa2ade
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-12-17 10:59:02 +0800

    Use rust's direction

--- src/mir/analysis.rs
+++ src/mir/analysis.rs
@@ -21,11 +21,45 @@ pub trait JoinSemiLattice: Eq {
     fn join(&mut self, other: &Self) -> bool;
 }
 
-/// Direction of an analysis
-#[derive(PartialEq, Eq)]
-pub enum Direction {
-    /// From first basic block to last one
-    FORWARD,
-    /// From last basic block to first one
-    BACKWARD,
+/// The direction of this analysis. Either `Forward` or `Backward`.
+pub trait Direction {
+    const IS_FORWARD: bool = !Self::IS_BACKWARD;
+
+    const IS_BACKWARD: bool = !Self::IS_FORWARD;
+}
+
+pub struct Forward;
+
+impl Direction for Forward {
+    const IS_FORWARD: bool = true;
+}
+
+pub struct Backward;
+
+impl Direction for Backward {
+    const IS_BACKWARD: bool = true;
+}
+
+/// Defines the domain of a dataflow problem.
+///
+/// This trait specifies the lattice on which this analysis operates (the domain) as well as its
+/// initial value at the entry point of each basic block.
+pub trait AnalysisDomain {
+    /// The type that holds the dataflow state at any given point in the program.
+    type Domain: Clone + JoinSemiLattice;
+
+    /// The direction of this analysis. Either `Forward` or `Backward`.
+    type Direction: Direction = Forward;
+
+    /// A descriptive name for this analysis. Used only for debugging.
+    ///
+    /// This name should be brief and contain no spaces, periods or other characters that are not
+    /// suitable as part of a filename.
+    const NAME: &'static str;
+
+    /// Returns the initial value of the dataflow state upon entry to each basic block.
+    fn bottom_value(&self, body: &Body) -> Self::Domain;
+
+    /// Mutates the initial value of the dataflow state upon entry to the `START_BLOCK`.
+    fn initialize_start_block(&self, body: &Body, state: &mut Self::Domain);
 }
