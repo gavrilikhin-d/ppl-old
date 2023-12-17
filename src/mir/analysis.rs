f4121dfb7ddca3507ab21a0cf027b0344a5a702d
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2023-12-17 10:49:14 +0800

    Add `JoinSemiLattice` trait

--- src/mir/analysis.rs
+++ src/mir/analysis.rs
@@ -8,6 +8,19 @@ use super::{
     local::LocalID,
 };
 
+/// A [partially ordered set][poset] that has a [least upper bound][lub] for any pair of elements
+/// in the set.
+///
+/// [lub]: https://en.wikipedia.org/wiki/Infimum_and_supremum
+/// [poset]: https://en.wikipedia.org/wiki/Partially_ordered_set
+pub trait JoinSemiLattice: Eq {
+    /// Computes the least upper bound of two elements, storing the result in `self` and returning
+    /// `true` if `self` has changed.
+    ///
+    /// The lattice join operator is abbreviated as `∨`.
+    fn join(&mut self, other: &Self) -> bool;
+}
+
 /// Direction of an analysis
 #[derive(PartialEq, Eq)]
 pub enum Direction {
