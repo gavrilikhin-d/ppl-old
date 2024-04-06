d2fb0df238a27d2980a2c8930fe5d88e9b698374
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2024-04-06 13:29:42 +0800

    Implement new command

--- src/driver/execute/new.rs
+++ src/driver/execute/new.rs
@@ -1,3 +1,6 @@
+use cmd_lib::run_cmd;
+use miette::miette;
+
 use crate::driver::commands::New;
 
 use super::Execute;
@@ -7,6 +10,12 @@ impl Execute for New {
 
     /// Compile single ppl file
     fn execute(&self) -> Self::Output {
+        const TEMPLATE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/src/driver/template");
+        let package = &self.package;
+        run_cmd!(
+            cp -r $TEMPLATE $package
+        )
+        .map_err(|e| miette!("{e}"))?;
         Ok(())
     }
 }
