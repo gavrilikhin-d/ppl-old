1e2b2e070351ad13a958aca6433ee3457748ebb3
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2024-05-19 16:18:30 +0800

    Revert "Make rational to be arc (#175)"
    This reverts commit 8c52c17b411f36b85567f80143e66567a6610409.

--- src/runtime/src/integer.rs
+++ src/runtime/src/integer.rs
@@ -140,7 +140,10 @@ pub extern "C" fn integer_slash_integer(x: Integer, y: Integer) -> Rational {
     let x = x.as_ref();
     let y = y.as_ref();
 
-    (rug::Rational::from(x) / y).into()
+    let boxed = Box::new(rug::Rational::from(x) / y);
+    Rational {
+        data: Box::into_raw(boxed),
+    }
 }
 
 /// Compare 2 integers for equality
