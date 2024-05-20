c78af823bdcb89f5cfff1b2f667ad7d6b231a25c
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2024-05-20 17:11:52 +0800

    Add cramer method and change b

--- benchmarks/linalg/main.py
+++ benchmarks/linalg/main.py
@@ -1,28 +1,30 @@
 import numpy as np
 import os
 
+from sympy import harmonic, Float
+
 # read n from N env
 n = int(os.environ.get('N', 10))
 
 # Define Hilbert matrix A
 A = np.array([[1.0 / (i + j + 1) for j in range(0, n)] for i in range(0, n)])
 
-# Define vector b
-b = np.ones(n)
+x = np.ones(n)
+
+# Define vector b through the harmonic numbers (this is better)
+b = np.array([Float(harmonic(n + i) - harmonic(i)).evalf() for i in range(0, n)], dtype=np.float64)
+# b = A @ x
 
 # Solve Ax = b
-x = np.linalg.solve(A, b)
-
-# # Round to nearest integer
-# x = np.round(x)
-
-# # Make it a vector of integers
-# x = x.astype(int)
+x_calc = np.linalg.solve(A, b)
 
 # Print vertically for easier reading
 print("Solution for x:")
 for i in range(0, n):
-    print(x[i])
+    print(x_calc[i])
 
-print("Check Ax = b")
-print(A @ x)
+print("|r| = |Ax_calc - b| = ", end="")
+print(np.linalg.norm(A @ x_calc - b))
+
+print("|z| = |x - x_calc| = ", end="")
+print(np.linalg.norm(x - x_calc))
